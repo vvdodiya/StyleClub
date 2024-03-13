146da6b5584cb8d25b6d1b712e19eb7a531710d1
Vishal Dodiya <v.v.dodiya101@gmail.com>
2024-03-13 22:18:57 +0530

    Login form commit

---
+++ StyleClub/StyleClub/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StyleClub.Models;
+
+namespace StyleClub.Controllers
+{
+    public class AdminController : Controller
+    {
+        StyleClubdbEntities db = new StyleClubdbEntities();
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Category()
+        {
+            return View();
+        }
+
+
+    }
+}
